ded58dd0763e1a27d8fb827da08bc620e518e148
Skr1P1 <78951885+Skr1P1@users.noreply.github.com>
2021-06-04 05:44:07 +0300

    vb

--- ind_2.py
+++ ind_2.py
@@ -1,11 +1,16 @@
-s = input('Введите предложение -->')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-from datetime import date
+# Вывести количество слов или
 
-word = s.split(' ')
-liters = ['или']
-n = 0
+if __name__ == '__main__':
+    s = input('Введите предложение -->')
 
-for i in word:
-    if i in liters:
-        n += 1
+    word = s.split(' ')
+    liters = ['или']
+    n = 0
 
-print('Кол-во слов "или":', n)
+    for i in word:
+        if i in liters:
+            n += 1
+
+    print('Кол-во слов "или":', n)
--- ind_3.py
+++ ind_3.py
@@ -1,2 +1,7 @@
-fall = 'рпроцессо'
-print(fall[1:] + fall[0])
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Дано предложение. Определить число букв о в нем.
+
+if __name__ == '__main__':
+    fall = 'рпроцессо'
+    print(fall[1:] + fall[0])
